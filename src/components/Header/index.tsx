d9ac71b6cf81253cc28a2c958fb5ca7102d0dad9
renchang.peng <renchang.peng@riversecurity.com>
2024-12-26 14:38:23 +0800

    feat(ui): 更新Header组件并新增Dashboard页面

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,12 @@ import "./style.scss";
 
 const Header: React.FC = () => {
   return (
-    <Layout.Header className="h-[56px] p-0 flex justify-between items-center headerContainer">
+    <Layout.Header className="h-[56px] p-0 flex justify-between items-center headerContainer text-white">
       <div className="logo"></div>
-      <div></div>
+      <div className="flex items-center justify-between flex-1">
+        <div>menu</div>
+        <div className="flex mr-8"></div>
+      </div>
     </Layout.Header>
   );
 };
