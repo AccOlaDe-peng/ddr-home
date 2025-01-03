edc84dfc06550de0202b1da6b8cf161879224361
renchang.peng <renchang.peng@riversecurity.com>
2025-01-03 11:36:26 +0800

    feat(Menu): 添加菜单状态管理与浏览器事件监听功能

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { IRoute, MenuRoute } from "@/router/routes";
 import { MenuItem } from "@/types/menu";
+import { Button, Flex } from "antd";
 // import { isMenuKeyFormStore } from "@/utils/tool/auth";
-import { useSetState } from "@reactuses/core";
+import { dropRight, has, isEmpty, isObject, join, last, split } from "lodash";
+import { useEventListener, useSetState } from "@reactuses/core";
 import { layoutRoutesConfig as routes } from "@/router/routes";
-import { useNavigate } from "react-router-dom";
-import { Button } from "antd";
+import { matchRoutes, useNavigate } from "react-router-dom";
 import { ItemType } from "antd/es/menu/interface";
-import { has, isObject } from "lodash";
 
 interface State {
   selectedKey: string;
@@ -16,7 +16,6 @@ interface State {
 const LayoutMenu: React.FC = () => {
   const navigate = useNavigate();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [state, setState] = useSetState<State>({
     selectedKey: "",
   });
@@ -24,52 +23,147 @@ const LayoutMenu: React.FC = () => {
   const menu: Array<MenuItem> = [];
 
   const getMenuData = (routes: IRoute[]) => {
-    const menuData: IRoute[] = [];
-    routes.forEach((route) => {
-      const _route = { ...route };
-      if (
-        _route.menuRender !== false
-        //  && isMenuKeyFormStore(menu, route.key!)
-      ) {
-        if (_route.children) {
-          _route.children = getMenuData(_route.children) as MenuRoute[];
+    return routes.reduce((menuData: IRoute[], route) => {
+      if (route.menuRender !== false) {
+        if (
+          route.menuRender !== false
+          //  && isMenuKeyFormStore(menu, route.key!)
+        ) {
+          const _route = { ...route };
+          if (_route.children) {
+            _route.children = getMenuData(_route.children) as MenuRoute[];
+          }
+          menuData.push(_route);
         }
-        menuData.push(_route);
       }
-    });
-    return menuData;
+      return menuData;
+    }, []);
   };
+
+  /**
+   * @description: 监听浏览器前进后退事件
+   * @return {*}
+   */
+  useEventListener(
+    "popstate",
+    () => {
+      const menuState = handleMenuState(location);
+      console.log(menuState);
+      updateMenuState(menuState);
+    },
+    window
+  );
 
   const menuData: IRoute[] = React.useMemo(() => getMenuData(routes), [menu]);
 
-  //   const updateMenuState = ({ selectedKey }: State) => {
-  //     setState(() => ({
-  //       selectedKey,
-  //     }));
-  //   };
+  /**
+   * @description: 更新菜单状态
+   * @param {State} param1
+   * @return {*}
+   */
+  const updateMenuState = useCallback(
+    ({ selectedKey }: State) => {
+      setState(() => ({
+        selectedKey,
+      }));
+    },
+    [setState]
+  );
+
+  /**
+   * @description: 初始监听路由变化
+   * @return {*}
+   */
+  useEffect(() => {
+    if (menuData.length === 0) {
+      return;
+    }
+    const menuState = handleMenuState(location);
+    console.log(menuState);
+    if (menuState.selectedKey !== state.selectedKey) {
+      updateMenuState(menuState);
+    }
+  }, [menuData, state.selectedKey, updateMenuState]);
+
   const menuConfig = generateMenuItems(menuData);
 
+  const handleSkipTo = (key: string) => {
+    setState({ selectedKey: key });
+    navigate(key);
+  };
+
+  const MenuButton = (item: ItemType) => {
+    const key = item?.key;
+    if (isObject(item) && has(item, "label")) {
+      const label = item.label as React.ReactNode;
+      return (
+        <Button
+          type="text"
+          style={{
+            color: state.selectedKey === key ? "#fff" : "#ccc",
+          }}
+          className="hover:bg-[#494f55]"
+          size="large"
+          key={key}
+          onClick={() => typeof key === "string" && handleSkipTo(key)}
+          onMouseEnter={(e) =>
+            (e.currentTarget.style.backgroundColor = "#494f55")
+          }
+          onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = "")}
+        >
+          {label}
+        </Button>
+      );
+    }
+    return null;
+  };
+
   return (
-    <div className="flex">
+    <Flex align="center" gap={10}>
       {menuConfig
         .filter((item) => item?.key !== "/")
-        .map((item) => {
-          const key = item?.key;
-          if (isObject(item) && has(item, "label")) {
-            const label = item.label as React.ReactNode;
-            return (
-              <Button
-                key={key}
-                onClick={() => typeof key === "string" && navigate(key)}
-              >
-                {label}
-              </Button>
-            );
-          }
-          return null;
-        })}
-    </div>
+        .map((item) => MenuButton(item))}
+    </Flex>
   );
+};
+
+/**
+ * @description: 处理菜单状态
+ * @param {Location} location
+ * @return {*}
+ */
+const handleMenuState = (location: Location) => {
+  const currentPageMatchRoutes = matchRoutes(routes, location);
+  if (!currentPageMatchRoutes) {
+    return { selectedKey: "" };
+  }
+  const selectRoute = last(currentPageMatchRoutes);
+  // //将isChild加入新的校验
+  // if (some(currentPageMatchRoutes, (r) => !isUndefined(r.route.isChild))) {
+  //     const rvRoutes = reverse(currentPageMatchRoutes);
+  //     const selectRouteIndex = findIndex(rvRoutes, (o) => !!o.route.isChild);
+  //     selectRoute = rvRoutes[selectRouteIndex];
+  //     openKeys = (map(rvRoutes, (i) => i.route.path).slice(selectRouteIndex, rvRoutes.length) ??
+  //         []) as string[];
+  // } else {
+  //     selectRoute = last(currentPageMatchRoutes);
+  //     openKeys = (currentPageMatchRoutes?.map((item) => item.route.path).slice(0, -1) ??
+  //         []) as string[];
+  // }
+
+  let selectedKey: string = selectRoute!.pathnameBase;
+
+  if (selectRoute?.route.menuRender === false) {
+    const parentPath = join(dropRight(split(selectRoute.route.path, "-")), "/");
+    if (!isEmpty(parentPath)) {
+      // const parentMatchRoute = matchRoutes(routes, parentPath);
+      // openKeys = (parentMatchRoute?.map((item) => item.route.path).slice(0, -1) ??
+      //     []) as string[];
+      selectedKey = parentPath;
+    }
+  }
+
+  return { selectedKey };
 };
 
 /**
